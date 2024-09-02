826764ecbc3b837b516a325a83c72a9302158e4c
Sumir Seth <iamsumirseth@gmail.com>
2024-09-03 01:35:24 +0530

    polish

--- pages/profile.vue
+++ pages/profile.vue
@@ -4,8 +4,6 @@
       <div class="py-8 sm:py-12 md:py-16 text-center flex flex-col items-center justify-center h-screen">
         <h1 class="text-3xl sm:text-4xl md:text-5xl font-bold mb-2 sm:mb-3 md:mb-4">Profile Page</h1>
         <p class="mb-4 sm:mb-6 md:mb-8">Email : {{ user?.email }}</p>
-        <UButton @click="testFunction">Logs</UButton>
-        
 
       </div>
     </UContainer>
@@ -16,10 +14,6 @@
 const user = useSupabaseUser()
 const client = useSupabaseClient()
 
-const testFunction = async() => {
-  const { data, error } = await client.from('todos').select('*')
-  console.log(data)
-}
 </script>
 
 <style>
